b26373da106872437032e9aefb4440bce87e5f2b
dalmi20 <f.kessaissia@esi-sba.dz>
2024-01-20 21:10:18 +0100

    auth v1

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,14 +4,35 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import {cn} from "@/lib/utils"
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
 
 export const Navbar = () => {
     const scrolled =useScrollTop()
+    const {isAuthenticated,isLoading} = useConvexAuth()
     return ( 
         <div className={cn("z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6 ",
         scrolled && "border-b shadow-sm")}>
             <Logo/>
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <p>Loading...</p>
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                    <SignInButton mode="modal">
+                        <Button variant="ghost" size="sm">
+                            Log in
+                        </Button>
+                    </SignInButton>
+                    <SignInButton mode="modal">
+                        <Button  size="sm">
+                            Get Jotion Free
+                        </Button>
+                    </SignInButton>
+                    </>
+                )}
                 <ModeToggle/>
             </div>
         </div>
---
+++ convex/auth.convex.js
@@ -0,0 +1,8 @@
+export default {
+    providers:[
+        {
+            domain:"https://brief-crab-76.clerk.accounts.dev",
+            applicationID:"convex",
+        }
+    ]
+}
