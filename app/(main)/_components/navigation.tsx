d905367701ce2c2c59fc53f20717137b3a3b2615
dalmi20 <f.kessaissia@esi-sba.dz>
2024-01-26 20:58:59 +0100

    Documents Page

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation"
 import { useRef , ElementRef ,useState,useEffect} from "react"
 import {useMediaQuery} from "usehooks-ts"
 import { cn } from "@/lib/utils"
+import { UserItem } from "./user-item"
 
 
 export const Navigation =() =>{
@@ -109,7 +110,7 @@ export const Navigation =() =>{
 
                     </div>
                     <div>
-                        <p>Action items</p>
+                        <UserItem />
                     </div>
 
                     <div className="mt-4">
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from 'sonner'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -40,6 +41,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey='jotion-theme-2'
           >
+            <Toaster position='bottom-center'/>
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
